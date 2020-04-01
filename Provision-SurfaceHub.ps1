514705eee3b81410d34114c46a3cdc6304f42e9c
richlilly2004 <rich.lilly@outlook.com>
2020-04-01 06:03:56 -0500

    Added Intune Policy configuration

--- Provision-SurfaceHub.ps1
+++ Provision-SurfaceHub.ps1
@@ -9,16 +9,17 @@
 #Create Surface Hub account
 #Set location usage & Meeting Room license
 #Create Dynamic AAD Device Group on OS = SurfaceHub
-#Provision Azure Log Analytics workspace and retrieve customerID & key
-#Configure Intune policies
+#Provision Azure Log Analytics workspace and retrieve customerID & key, Install SurfaceHub Solution
+#Configure Intune policies for Surface Hub and set LA tenant info
 
 #PowerShell modules required
 #AzureADPreview
 #Azure
+#Microsoft.Graph.Intune
 
 #User Input Required
 $Credential = Get-Credential
-$UPN = "Test03252020-12@netrixebc.com"
+$UPN = "Test03252020-20@netrixebc.com"
 $usagelocation = "US"
 $workspacename = "netrixebchub"
 $ResourceGroupName = "USE-SurfaceHub-RG"
@@ -34,6 +35,7 @@ $params1 = @{"OwnerEmail"="$emailowner"}
 $365Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://ps.outlook.com/powershell -Credential $Credential -Authentication Basic –AllowRedirection
 $ImportResults = Import-PSSession $365Session -AllowClobber
 Connect-AzureAD
+Connect-MSGraph -Credential $Credential
 Add-AzAccount
 $subscription = get-azsubscription |out-gridview -passthru
 Select-azsubscription -subscription $subscription
@@ -68,11 +70,17 @@ New-AzResourceGroup -Name $ResourceGroupName -Location $RGLocation -Tag $params1
 $LAWorkspace = New-AzOperationalInsightsWorkspace -ResourceGroupName $ResourceGroupName -Name $workspacename -Location $RGLocation -Sku standalone
 $LAWorkspaceCustomerId = $LAWorkspace.CustomerId.Guid
 $LAWorkspaceKey = (Get-AzOperationalInsightsWorkspaceSharedKey -ResourceGroupName $ResourceGroupName -Name $workspacename).PrimarySharedKey
+Set-AzOperationalInsightsIntelligencePack -ResourceGroupName $ResourceGroupName -WorkspaceName $workspacename -IntelligencePackName SurfaceHub -Enabled $true
 
 #Configure Intune policies
+$IntuneHubPolicy1 = New-IntuneDeviceConfigurationPolicy -displayName "Surface Hub Microsoft Teams" -windows10TeamGeneralConfiguration -azureOperationalInsightsBlockTelemetry $false -azureOperationalInsightsWorkspaceId $LAWorkspaceCustomerId -azureOperationalInsightsWorkspaceKey $LAWorkspaceKey -welcomeScreenMeetingInformation showOrganizerAndTimeAndSubject -maintenanceWindowStartTime 00:00:00.0000000 -maintenanceWindowDurationInHours 4
+New-IntuneDeviceConfigurationPolicyAssignment -deviceConfigurationId $IntuneHubPolicy1.id -target (New-Object PSObject -Property ([Ordered]@{'@odata.type' = '#microsoft.graph.groupAssignmentTarget'; groupId = $group.Id}))
 
+#$IntuneHubPolicy2 = New-IntuneDeviceConfigurationPolicy -displayName "Surface Hub Microsoft Teams" -omaSettings (New-Object PSObject -Property ([Ordered]@{'@odata.type' = '#microsoft.graph.omaSettingInteger'; 'displayName' = 'Teams Mode'; 'omaUri' = './Vendor/MSFT/SurfaceHub/Properties/SurfaceHubMeetingMode'; 'value' = '1'}))
+#New-IntuneDeviceConfigurationPolicyAssignment -deviceConfigurationId $IntuneHubPolicy2.id -target (New-Object PSObject -Property ([Ordered]@{'@odata.type' = '#microsoft.graph.groupAssignmentTarget'; groupId = $group.Id}))
 
 ##Clean-up
 #Remove-Mailbox -Identity $UPN -Confirm
 #Remove-AzureADMSGroup -Id $group.Id
 #Remove-AzResourceGroup -Name $ResourceGroupName
+#Remove-IntuneDeviceConfigurationPolicy -deviceConfigurationId $IntuneHubPolicy1.deviceConfigurationId
